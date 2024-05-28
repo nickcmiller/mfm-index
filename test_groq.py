fa973e0ee444edcd1a82116111caac7f095fc1a5
Nick Miller <miller.nick.c@gmail.com>
2024-05-28 03:40:55 +0000

    finalize test_groq

--- test_groq.py
+++ test_groq.py
@@ -7,6 +7,7 @@ import shutil
 from dotenv import load_dotenv
 import time
 import httpx
+import json
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv('.env')
@@ -19,7 +20,7 @@ def groq_transcribe_audio(audio_chunk_file: str) -> str:
         audio_chunk_file (str): The path to the audio chunk file to be transcribed.
 
     Returns:
-        str: The transcribed text
+        str: JSON with the transcribed audio.
     """
     client = Groq()
     max_retries = 6
@@ -38,9 +39,10 @@ def groq_transcribe_audio(audio_chunk_file: str) -> str:
                     transcript_chunk_file = client.audio.transcriptions.create(
                         file=(audio_chunk_file, af.read()),
                         model="whisper-large-v3",
+                        response_format="verbose_json"
                     )
                 time.sleep(3)
-                return transcript_chunk_file.text
+                return json.dumps(transcript_chunk_file.segments, indent=4)
             except Exception as e:
                 if "429" in str(e) or "rate_limit_exceeded" in str(e):
                     logging.info(f"Received 429 response, waiting {retry_delay} seconds before retrying... (Attempt {attempt + 1}/{max_retries})")
@@ -63,3 +65,21 @@ def groq_transcribe_audio(audio_chunk_file: str) -> str:
     except Exception as e:
         logging.error(f"Unexpected error occurred: {e}")
         raise
+
+def format_response(response: json):
+    segments = []
+    for segment in response:
+        start_time = segment["start"]
+        end_time = segment["end"]
+        text = segment["text"]
+        segments.append({
+            "start_time": start_time, 
+            "end_time": end_time, 
+            "text": text.strip()
+        })
+    return segments
+
+if __name__ == "__main__":
+    response = groq_transcribe_audio('Former OPD Chief LeRonne Armstrong announces city council run.mp3')
+    parsed_response = json.loads(response)
+    print(format_response(parsed_response))
