d35e26b6bd5b2938f5a954a1adb858b40d745d9c
Nick Miller <miller.nick.c@gmail.com>
2024-05-28 05:58:31 +0000

    add some chunking

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,4 +1,5 @@
 from groq import Groq
+from pydub import AudioSegment
 import os
 from typing import List, Optional, Dict
 import logging
@@ -12,12 +13,12 @@ import json
 logging.basicConfig(level=logging.INFO)
 load_dotenv('.env')
 
-def groq_transcribe_audio(audio_chunk_file: str) -> str:
+def groq_transcribe_audio(audio_file: str) -> str:
     """
-    This function transcribes an audio chunk file using the Groq library.
+    This function transcribes an audio file using the Groq library.
 
     Args:
-        audio_chunk_file (str): The path to the audio chunk file to be transcribed.
+        audio_file (str): The path to the audio file to be transcribed.
 
     Returns:
         str: JSON with the transcribed audio.
@@ -29,20 +30,20 @@ def groq_transcribe_audio(audio_chunk_file: str) -> str:
     # Transcribe the audio file
     try:
         # Get the file size in megabytes and log it
-        file_size_bytes = os.path.getsize(audio_chunk_file)
+        file_size_bytes = os.path.getsize(audio_file)
         file_size_megabytes = file_size_bytes / (1024 * 1024)
         logging.info(f"The size of the file sent to Groq Whisper is {file_size_megabytes} MB.")
 
         for attempt in range(max_retries):
             try:
-                with open(audio_chunk_file, "rb") as af:
-                    transcript_chunk_file = client.audio.transcriptions.create(
-                        file=(audio_chunk_file, af.read()),
+                with open(audio_file, "rb") as af:
+                    transcription= client.audio.transcriptions.create(
+                        file=(audio_file, af.read()),
                         model="whisper-large-v3",
                         response_format="verbose_json"
                     )
                 time.sleep(3)
-                return json.dumps(transcript_chunk_file.segments, indent=4)
+                return transcription
             except Exception as e:
                 if "429" in str(e) or "rate_limit_exceeded" in str(e):
                     logging.info(f"Received 429 response, waiting {retry_delay} seconds before retrying... (Attempt {attempt + 1}/{max_retries})")
@@ -54,7 +55,7 @@ def groq_transcribe_audio(audio_chunk_file: str) -> str:
                         return None
                     time.sleep(retry_delay)
     except FileNotFoundError:
-        logging.error(f"File {audio_chunk_file} not found.")
+        logging.error(f"File {audio_file} not found.")
         raise
     except httpx.ConnectError as e:
         logging.error(f"Connection error occurred: {e}")
@@ -66,21 +67,88 @@ def groq_transcribe_audio(audio_chunk_file: str) -> str:
         logging.error(f"Unexpected error occurred: {e}")
         raise
 
-def format_response(response: str) -> List[dict]:
-    parsed_response = json.loads(response)
+def format_response(transcription: str, added_duration: float=0.0) -> List[dict]:
+    if isinstance(transcription, list):
+        transcription = json.dumps(transcription)
 
     segments = []
-    for segment in parsed_response:
+    for segment in transcription.segments:
         start_time = segment["start"]
         end_time = segment["end"]
         text = segment["text"]
         segments.append({
-            "start_time": start_time, 
-            "end_time": end_time, 
+            "start_time": start_time + added_duration, 
+            "end_time": end_time + added_duration, 
             "text": text.strip()
         })
+    return json.dumps(segments, indent=4)
+
+def create_audio_chunks(audio_file: str, temp_dir: str, chunk_size: int=25*60000) -> List[str]:
+    """
+    Splits an audio file into smaller segments or chunks based on a specified duration. This function is useful for processing large audio files incrementally or in parallel, which can be beneficial for tasks such as audio analysis or transcription where handling smaller segments might be more manageable.
+
+    AudioSegment can slice an audio file by specifying the start and end times in milliseconds. This allows you to extract precise segments of the audio without needing to process the entire file at once. For example, `audio[1000:2000]` extracts a segment from the 1-second mark to the 2-second mark of the audio file.
+
+    Args:
+        audio_file (str): The absolute or relative path to the audio file that needs to be chunked. This file should be accessible and readable.
+        
+        chunk_size (int): The length of each audio chunk expressed in milliseconds. This value determines how the audio file will be divided. For example, a `chunk_size` of 1000 milliseconds will split the audio into chunks of 1 second each.
+        
+        temp_dir (str): The directory where the temporary audio chunk files will be stored. This directory will be used to save the output chunk files, and it must have write permissions. If the directory does not exist, it will be created.
+
+    Returns:
+        List[str]: A list containing the file paths of all the audio chunks created. Each path in the list represents a single chunk file stored in the specified `temp_dir`. The files are named sequentially based on their order in the original audio file.
+
+    Raises:
+        FileNotFoundError: If the `audio_file` does not exist or is inaccessible.
+        
+        PermissionError: If the script lacks the necessary permissions to read the `audio_file` or write to the `temp_dir`.
+        ValueError: If `chunk_size` is set to a non-positive value.
+    """
+    os.makedirs(temp_dir, exist_ok=True)
+    file_name = os.path.splitext(os.path.basename(audio_file))[0]
+
+    try:
+        audio = AudioSegment.from_file(audio_file)
+    except Exception as e:
+        logging.error(f"create_audio_chunks failed to load audio file {audio_file}: {e}")
+        logging.error(traceback.format_exc())
+        return []
+
+    start = 0
+    end = chunk_size
+    counter = 0
+    chunk_files = []
+
+    while start < len(audio):
+        chunk = audio[start:end]
+        chunk_file_path = os.path.join(temp_dir, f"{counter}_{file_name}.mp3")
+        try:
+            chunk.export(chunk_file_path, format="mp3") # Using .mp3 because it's cheaper
+            chunk_files.append(chunk_file_path)
+        except Exception as e:
+            error_message = f"create_audio_chunks failed to export chunk {counter}: {e}"
+            logging.error(error_message)
+            logging.error(traceback.format_exc())
+            raise error_message
+        start += chunk_size
+        end += chunk_size
+        counter += 1
+    return chunk_files
+
+def transcribe_chunks(chunk_files: List[str]) -> str:
+    duration = 0
+    segments = []
+    for chunk_file in chunk_files:
+        response = groq_transcribe_audio(chunk_file)
+        segments.append(format_response(response, duration))
+        duration += response.duration
     return segments
 
 if __name__ == "__main__":
-    response = groq_transcribe_audio('Former OPD Chief LeRonne Armstrong announces city council run.mp3')    
-    print(format_response(response))
+    from download_video import yt_dlp_download
+    url = "https://www.youtube.com/watch?v=wz6-0EPBvqE&ab_channel=IndianaPacers"
+    audio_file = yt_dlp_download(url)
+    chunks = create_audio_chunks(audio_file, "temp")
+    segments = transcribe_chunks(chunks)
+    print(segments)
