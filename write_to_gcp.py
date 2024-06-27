9bd410c34861a44d3275d0ce2b27042a1f971e8a
Nick Miller <miller.nick.c@gmail.com>
2024-06-27 20:55:06 +0000

    fix instance connection formatting

--- write_to_gcp.py
+++ write_to_gcp.py
@@ -21,7 +21,7 @@ connector = Connector()
 print(f"type(sql_host): {type(sql_host)}")
 def getconn():
     conn = connector.connect(
-        instance_connection_string=str(sql_host),
+        instance_connection_string=sql_host,
         user=sql_user,
         password=sql_password,
         db=sql_database,
@@ -36,14 +36,14 @@ engine = sqlalchemy.create_engine(
 )
 
 table_name = 'new_table'
-data_object = {'column1': [1, 2, 3], 'column2': ['a', 'b', 'c']}
+data_object = {'column1': [1, 2, 3], 'column2': ['e', 'f', 'g']}
 df_to_write = pd.DataFrame(data_object)
 
 # Write the data to the table
 df_to_write.to_sql(
     table_name, 
     engine, 
-    if_exists='replace', 
+    if_exists='append', 
     index=False
 )
 print(f"Data written to table {table_name}")
