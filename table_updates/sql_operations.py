560bba5435da8709b9d08ccdfde4dd61cde2589b
Nick Miller <miller.nick.c@gmail.com>
2024-08-22 19:01:11 +0000

    add functions to find and delete rows that are just ads in the podcast

--- table_updates/sql_operations.py
+++ table_updates/sql_operations.py
@@ -5,10 +5,12 @@ import json
 import numpy as np
 from typing import List, Dict, Any
 
-from genai_toolbox.helper_functions.string_helpers import retrieve_file
+from genai_toolbox.helper_functions.string_helpers import retrieve_file, write_to_file
+from genai_toolbox.chunk_and_embed.embedding_functions import create_openai_embedding
+
 from gcp_postgres_pgvector.config.gcp_sql_config import load_config
 from gcp_postgres_pgvector.databases.connection import get_db_engine
-from gcp_postgres_pgvector.databases.operations import ensure_pgvector_extension, write_list_of_objects_to_table, read_from_table, read_similar_rows, delete_table
+from gcp_postgres_pgvector.databases.operations import ensure_pgvector_extension, write_list_of_objects_to_table, read_from_table, read_similar_rows, delete_table, delete_rows_by_ids
 from gcp_postgres_pgvector.utils.logging import setup_logging
 
 logger = setup_logging()
@@ -196,21 +198,53 @@ def delete_table_if_exists(
         except Exception as e:
             logger.error(f"Error deleting table '{table_name}': {e}", exc_info=True)
 
+def delete_rows_with_ids(
+    table_name: str,
+    ids: List[Any],
+    config=config
+) -> int:
+    """
+        Delete rows from the specified table based on a list of IDs.
+
+        Args:
+            table_name (str): The name of the table to delete rows from.
+            ids (List[Any]): A list of IDs to delete.
+            config (dict): A dictionary containing the database configuration parameters.
+
+        Returns:
+            int: The number of rows deleted.
+
+        Raises:
+            Exception: If there's an error during the deletion process.
+    """
+    print(f"Config in delete_rows_by_ids: {config}")
+    logger.info(f"Deleting rows from table '{table_name}' with IDs: {ids}")
+    with get_db_engine(config) as engine:
+        try:
+            ensure_pgvector_extension(engine)
+            deleted_count = delete_rows_by_ids(engine, table_name, ids)
+            logger.info(f"Successfully deleted {deleted_count} rows from table '{table_name}'")
+            return deleted_count
+        except Exception as e:
+            logger.error(f"Failed to delete rows from table '{table_name}': {e}", exc_info=True)
+            raise
+
 def load_and_process_data(
+    file_name: str = "test_embeddings.json",
+    dir_name: str = "tmp"
 ) -> List[Dict[str, Any]]:
     """
-    Load and process data from a JSON file.
+        Load and process data from a JSON file.
 
-    This function retrieves data from a JSON file named 'test_embeddings.json' in the 'tmp' directory,
-    processes it, and returns a subset of the data for further use. The function prints the types of each key-value pair in the first data object (excluding 'embedding'). The full data is loaded, but only a subset is returned to limit memory usage and processing time.
+        This function retrieves data from a JSON file named 'test_embeddings.json' in the 'tmp' directory,
+        processes it, and returns a subset of the data for further use. The function prints the types of each key-value pair in the first data object (excluding 'embedding'). The full data is loaded, but only a subset is returned to limit memory usage and processing time.
 
-    Returns:
-        list: A list containing the first 6 items from the loaded and processed data.
-
+        Returns:
+            list: A list containing the first 6 items from the loaded and processed data.
     """
     aggregated_chunked_embeddings = retrieve_file(
-        file="test_embeddings.json",
-        dir_name="tmp"
+        file=file_name,
+        dir_name=dir_name
     )
     
     data_object = aggregated_chunked_embeddings[0]
@@ -218,6 +252,70 @@ def load_and_process_data(
     for key, value in filtered_data_object.items():
         logger.info(f"{key}: {type(value)}")
     return aggregated_chunked_embeddings
+
+def save_similar_search_to_json(
+    table_name,
+    query_text,
+    model_choice="text-embedding-3-large",
+    config=config,
+    limit=100,
+    dir_name="tmp",
+    file_name="similar_search_results.json"
+):
+    """
+        Save the results of a cosine similarity search to a JSON file.
+
+        This function performs a cosine similarity search on the specified table and saves the results to a JSON file.
+        The results are stored in a dictionary with the IDs as keys and the texts as values.
+
+        Args:
+            table_name (str): The name of the table to search in.
+            query_text (str): The text to compare against.
+            model_choice (str): The model to use for embedding.
+            dir_name (str): The directory to save the results to.
+            file_name (str): The name of the file to save the results to.
+            config (dict): A dictionary containing the database configuration parameters.
+            limit (int): The maximum number of similar rows to return.
+    """
+
+    query_embedding = create_openai_embedding(
+        text=query_text,
+        model_choice=model_choice
+    )
+
+    search_results = cosine_similarity_search(
+        table_name, 
+        query_embedding,
+        config=config,
+        limit=limit
+    )
+
+    search_dict = {}
+    for result in search_results:
+        search_dict[result['id']] = result['text']
+
+    write_to_file(
+        search_dict,
+        file_name=file_name,
+        dir_name=dir_name
+    )
+
+def delete_json_results(
+    dir_name="tmp",
+    file_name="similar_search_results.json"
+):
+    results = retrieve_file(
+        file_name=file_name,
+        dir_name=dir_name
+    )
+    ids = list(results.keys())
+    
+    delete_rows_with_ids(
+        table_name, 
+        ids, 
+        config=config
+    )
+
 
 def main(
     operation, 
@@ -260,11 +358,28 @@ def main(
     logger.info("Main function completed")
 
 if __name__ == "__main__":
-    import argparse
+    table_name = 'vector_table'
 
-    parser = argparse.ArgumentParser(description="Process some arguments.")
-    parser.add_argument("operation", type=str, help="Specify the operation to perform")
+    # import argparse
 
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description="Process some arguments.")
+    # parser.add_argument("operation", type=str, help="Specify the operation to perform")
 
-    main(args.operation, table_name='vector_table')
+    # args = parser.parse_args()
+
+    # main(args.operation, table_name='vector_table')
+
+    if False:
+        text = "Let's get back to the show"
+
+        save_similar_search_to_json(
+            table_name,
+            query_text=text,
+            file_name="similar_search_results.json",
+            dir_name="tmp"
+        )
+    else:
+        delete_json_results(
+            file_name="similar_search_results.json",
+            dir_name="tmp"
+        )
